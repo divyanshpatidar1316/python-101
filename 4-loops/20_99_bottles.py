65dfed106b30c32c7eef2916b6642595d9930ed2
divyanshpatidar1316 <152731777+divyanshpatidar1316@users.noreply.github.com>
2025-07-10 01:12:30 +0530

    Update 20_99_bottles.py

--- 4-loops/20_99_bottles.py
+++ 4-loops/20_99_bottles.py
@@ -1,8 +1,12 @@
 # 99 Bottles of Beer 🍻
 # Codédex
 
-for i in range(99, 0, -1):
+for i in range (99,0,-1):
   print(f'{i} bottles of beer on the wall')
   print(f'{i} bottles of beer')
   print('Take one down, pass it around')
-  print(f'{i-1} bottles of beer on the wall')
+  if i - 1 !=0:
+    print(f'{i-1} bottles of beer on the wall')
+  else:
+    print('No more bottles of beer on the wall')
+  print('\n')
